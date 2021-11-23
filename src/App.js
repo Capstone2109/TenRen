5e9687c77cd2f882a6a7e51fa98637e59a2d30ba
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-11-23 02:09:29 -0500

    created main components and imported antd into the index.js

--- src/App.js
+++ src/App.js
@@ -37,9 +37,18 @@ const App = () => {
             </Switch>
           </div>
         </Layout>
+        <div className='footer'>
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center'}}>
+          Robin Noob <br />
+          All rights reserved
+        </Typography.Title>
+        <Space>
+          <Link to='/'>Home</Link>
+          <Link to='/exchanges'>Exchanges</Link>
+          <Link to='/news'>News</Link>
+        </Space>
       </div>
-      <div classname='footer'>
-      </div>
+    </div>
     </div>
   )
 }
