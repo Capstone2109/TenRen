4581d14ba0d49e4e74ffa34ff409e33d96fe165b
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-11-23 02:38:30 -0500

    created a store and imported Provider into index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import 'antd/dist/antd.css';
+import store from './app/store.js';
 import App from './App';
 
 
 ReactDOM.render(
   <Router>
+    <Provider store={store}>
     <App />
+    </Provider>
   </Router>
   ,document.getElementById('root'));
