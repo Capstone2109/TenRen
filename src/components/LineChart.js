c1db04bc5db114827d794e9b79f3b72f12f18823
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-11-23 20:58:38 -0500

    charts are working

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
+import Chart from 'chart.js/auto'
 import { Col, Row, Typography } from 'antd';
 
 const { Title } = Typography;
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
 
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinPrice.push(coinHistory?.data?.history[i].price);
+  }
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
+  }
+
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: 'Price In USD',
+        data: coinPrice,
+        fill: false,
+        backgroundColor: '#0071bd',
+        borderColor: '#0071bd',
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
   return (
     <>
       <Row className="chart-header">
@@ -15,8 +50,10 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
           <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
         </Col>
       </Row>
+      <Line data={data} options={options} />
     </>
   );
 };
 
 export default LineChart;
+
