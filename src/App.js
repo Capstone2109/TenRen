d16eab5461a29671e5e6ae071dedfd62decd2219
Shashi Sasitahran <shashi.sasitharan@gmail.com>
2021-11-30 08:15:46 -0500

    login route added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Homepage from './components/Homepage';
 import Cryptocurrencies from './components/Cryptocurrencies';
 import News from './components/News';
 import CryptoDetails from './components/CryptoDetails';
+import CryptoLogin from './components/LogIn';
 import './App.css';
 
 const App = () => {
@@ -33,6 +34,9 @@ const App = () => {
               </Route>
               <Route exact path='/news'>
                 <News />
+              </Route>
+              <Route exact path='/login'>
+                <CryptoLogin />
               </Route>
             </Switch>
           </div>
