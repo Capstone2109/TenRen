d5adf2178a4e2914d7283c62647ed2daaa3f291f
Shashi Sasitahran <shashi.sasitharan@gmail.com>
2021-11-30 07:39:43 -0500

    Boilerplate password check

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -26,3 +26,12 @@ const User = db.define("user", {
       default: false,
     },
   });
+
+/**
+ * instanceMethods
+ */
+User.prototype.correctPassword = function (candidatePwd) {
+    //we need to compare the plain version to an encrypted version of the password
+    return bcrypt.compare(candidatePwd, this.password);
+  };
+  
