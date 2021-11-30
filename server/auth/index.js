263be1890f3dcc7f85f857798aebf0077eb3053d
Shashi Sasitahran <shashi.sasitharan@gmail.com>
2021-11-30 09:51:20 -0500

    Basic authentication added

---
+++ server/auth/index.js
@@ -0,0 +1,37 @@
+const router = require("express").Router();
+const { User } = require("../db");
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const token = await User.authenticate(req.body);
+    console.log(token);
+    res.send({ token: await User.authenticate(req.body) });
+  } catch (err) {
+    next(err);
+  }
+});
+//todo need way to populate other fiields
+router.post("/signup", async (req, res, next) => {
+  try {
+    //only take in username and password fields ignore everything else
+    const { username, password } = req.body;
+    const user = await User.create({ username, password });
+    res.send({ token: await user.generateToken() });
+  } catch (err) {
+    if (err.name === "SequelizeUniqueConstraintError") {
+      res.status(401).send("User already exists");
+    } else {
+      next(err);
+    }
+  }
+});
+
+router.get("/me", async (req, res, next) => {
+  try {
+    res.send(await User.findByToken(req.headers.authorization));
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+module.exports = router;
