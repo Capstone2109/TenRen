9dee50bc2a08ec8ce269943f7ab5e54c5a4c4b85
Wilson Voltaire <wilsonvoltaire@gmail.com>
2021-12-01 17:52:43 -0500

    added express routes fix

--- src/app/auth.js
+++ src/app/auth.js
@@ -32,6 +32,7 @@ export const authenticate =
   (username, password, method) => async (dispatch) => {
     try {
       const res = await axios.post(`/auth/${method}`, { username, password });
+      console.log("Token",res.data.token)
       window.localStorage.setItem(TOKEN, res.data.token);
       dispatch(me());
     } catch (authError) {
